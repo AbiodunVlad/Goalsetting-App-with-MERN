d9952b3c22ab3d9bbd8789840733fdbad6205916
Abiodun Vlad <omonijoabiodun@gmail.com>
2025-10-15 20:27:10 +0100

    server.js for NextJS

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+const next = require("next");
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const colors = require("colors");
@@ -8,16 +10,55 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 
-const app = express();
+// const app = express();
 
-app.use(cors());
+// app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/goals", require("./routes/goalRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
+// app.use("/api/goals", require("./routes/goalRoutes"));
+// app.use("/api/users", require("./routes/userRoutes"));
 
-app.use(errorHandler);
+// // Serve frontend
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve((__dirname, "../", "frontend", "build", "index.html"))
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) =>
+//     res.send("Go to the .env file and set NODE_ENV to production.")
+//   );
+// }
+
+// app.use(errorHandler);
+
+// app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// THE BELOW WORKS FOR NEXTJS SPECIFICALLY WHILE THE ABOVE WORKS FOR CREATE REACT APP
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev, dir: "./frontend" });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: false }));
+
+  server.use("/api/goals", require("./routes/goalRoutes"));
+  server.use("./api/users", require("./routes/userRoutes"));
+
+  // NextJS handles frontend routes
+  server.all(/.*/, (req, res) => {
+    return handle(req, res);
+  });
+
+  server.use(errorHandler);
+
+  server.listen(port, () => console.log(`Server started on port ${port}`));
+});
